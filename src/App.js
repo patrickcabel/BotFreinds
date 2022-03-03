4b25ea05bd7bfd1e124103101df76350385e372d
patrickcabel <94148201+patrickcabel@users.noreply.github.com>
2022-03-03 12:55:15 -0700

    moved code to render in App.js

--- src/App.js
+++ src/App.js
@@ -15,16 +15,15 @@ class App extends Component {
 
   onsearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
-    console.log(event);
+  };
+
+  render() {
     const filteredRobots = this.state.robots.filter((robots) => {
       return robots.name
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    console.log(filteredRobots);
-  };
 
-  render() {
     return (
       <div className="tc">
         <h1>BotFriends</h1>;
